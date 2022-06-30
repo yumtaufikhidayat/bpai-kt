c49215897ed68542bad258ccbab1c99ca17f05f8
yumtaufikhidayat <taufik.hidayat@kpvendor.id>
2022-07-01 06:05:19 +0700

    Rename class.

--- app/src/main/java/com/taufik/ceritaku/SplashScreenActivity.kt
+++ app/src/main/java/com/taufik/ceritaku/SplashScreenActivity.kt
@@ -3,9 +3,9 @@ package com.taufik.ceritaku
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_screen)
     }
 }
